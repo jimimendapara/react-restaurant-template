c4fd0b3cc6c9c5526dfb25b41ce22cf39a4b75bb
Jimi Mendapara <jimmymendapara91@gmail.com>
2022-02-17 09:49:58 +1100

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MountLineRestaurent} from 'react-icons/md';
+
+import images from '../../constants/images';
+import './Navbar.css';
+
+
+const Navbar = () => (
+  <nav className='app__navbar'>
+    <div>
+      <img src={images.gericht} alt="app Logo" />
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'><a href='#home'>Home</a></li>
+      <li className='p__opensans'><a href='#about'>About</a></li>
+      <li className='p__opensans'><a href='#menu'>Menu</a></li>
+      <li className='p__opensans'><a href='#awards'>Awards</a></li>
+      <li className='p__opensans'><a href='#contact'>Contact</a></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
